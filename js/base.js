7c19691e4f5eeb88765a049d34aa81edac16de2a
James Mitchell <mail@jpmitchell.ca>
2025-12-24 01:04:53 -0500

    [ 2025-12-24 ] touch ups

--- js/base.js
+++ js/base.js
@@ -23,10 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function openPopup() { 
         bodyOverlay.style.display = "flex"; 
+        bodyOverlay.classList.add("overlay-is-open"); 
+        document.body.classList.add("overlay-is-open"); 
     }
 
     function closePopup() { 
         bodyOverlay.style.display = "none";
+        bodyOverlay.classList.remove("overlay-is-open"); 
+        document.body.classList.remove("overlay-is-open"); 
     }
 
     // openPopup(); 
@@ -85,42 +89,23 @@ document.addEventListener('DOMContentLoaded', () => {
 /// Daily Quote 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const quoteArray = [ 
-        { author: "Charlie Brown", text: "In the book of life, the answers aren't in the back." },
-        { author: "Charlie Brown", text: "That's the secret to life... replace one worry with another." },
-        { author: "Charlie Brown", text: "I've developed a new philosophy. I only dread one day at a time." },
-        { author: "Charlie Brown", text: "I love the kinds of hugs where you can physically feel the sadness leaving your body." },
-        { author: "Charlie Brown", text: "Nothing takes the taste out of peanut butter quite like unrequited love." },
-        { author: "Charlie Brown", text: "I think I'm afraid to be happy, every time I'm happy something bad happens." },
-        { author: "Charlie Brown", text: "My anxieties have anxieties." },
-        { author: "Charlie Brown", text: "For one brief moment today I thought I was winning in the game of life. But there was a flag on the play!" },
-        { author: "Charlie Brown", text: "Sometimes you lie in bed at night, and you don't have a single thing to worry about... That always worries me!" },
-        { author: "Charles M. Schulz", text: "I think I'm allergic to morning." },
-        { author: "Charles M. Schulz", text: "My life has no purpose, no direction, no aim, no meaning, and yet I'm happy. I can't figure it out. What am I doing right?" },
-        { author: "Charles M. Schulz", text: "Be yourself. No one can say you're doing it wrong." },
-        { author: "Charles M. Schulz", text: "The less you want, the more you love." },
-        { author: "Charles M. Schulz", text: "Few people are successful unless other people want them to be." },
-        { author: "Lucy van Pelt", text: "All I really need is love, but a little chocolate now and then doesn't hurt." },
-        { author: "Lucy van Pelt", text: "Ugh! I’ve been kissed by a dog! I have dog germs! Get hot water! Get some disinfectant! Get some Iodine!" },
-        { author: "Lucy van Pelt", text: "Happiness is a warm puppy." },
-        { author: "Lucy van Pelt", text: "I never made a mistake in my life. I thought I did once, but I was wrong." },
-        { author: "Lucy van Pelt", text: "Try not to have a good time... This is supposed to be educational." },
-        { author: "Linus van Pelt", text: "No problem is so big or so complicated that it can't be run away from!" },
-        { author: "Linus van Pelt", text: "Big sisters are the crab grass in the lawn of life." },
-        { author: "Linus van Pelt", text: "Don't be a leaf... Be a tree!" },
-        { author: "Sally Brown", text: "I think I've discovered the secret of life - you just hang around until you get used to it." },
-        { author: "Sally Brown", text: "Light travels at a speed of 186,000 miles per second…So why are the afternoons so long?" },
-        { author: "Snoopy", text: "There's no sense in doing a lot of barking if you don't really have anything to say." },
-        { author: "Frieda", text: "People always expect more of you when you have naturally curly hair!" },
-    ]
-    
+   
     const today = new Date();
     const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 86400000)+3;
     const seed = dayOfYear * 9301 + 49297;
     const randomIndex = seed % quoteArray.length;
     const randomQuote = quoteArray[randomIndex];
-    const quote = document.querySelector("daily-quote"); 
-    quote.innerHTML = `${randomQuote.text} <br><br> - ${randomQuote.author}`;
+    const quoteContent = document.querySelector("daily-quote-content"); 
+    const quoteTitle = document.querySelector("daily-quote-title"); 
+    const dateString = today.toLocaleDateString(undefined, {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+    });
+
+    quoteContent.innerHTML = `${randomQuote.text} <br> - ${randomQuote.author}`;
+    quoteTitle.innerHTML =  `For ${dateString}`
 });
 
 
